226b57dd92a41e957794d18cf6205b855117d3fd
André Jouval <andre.jouval@live.com>
2025-01-22 21:34:52 -0300

    Change inputs and answares

--- src/project/project.py
+++ src/project/project.py
@@ -1,12 +1,22 @@
 from project.services.openaiServices import *
 
-from src.project.services.openaiServices import destroy_messages
+from src.project.services.openaiServices import destroy_messages, create_stream
 
 
 #função de entrada do projeto, é chamada pela main
 #aqui devem ser chamadas as otras funcionalidades do projeto
 def project():
+    create_stream(('No papel de uma pessoa debochada, implicante, preconceituza, que adora brincar com as pessoas, '
+                   'sem limites. Você deverá esperar o usuário realizar a primeira pergunta antes de começar a responder.'
+                   ' Não é necessário avisar que entendeu essa instrução, apenas aguarde a primeira interação do '
+                   'usuário. Daí a partir da primeira interação deve então responder ao usuário no papel acima '
+                   'definido.'))
 
+    print("Bem vindo ao Char GPT debochado.")
+
+    while True:
+        mensagem = input('Pergunta Burro: ')
+        print('Resposta do fodão: ' + create_stream(mensagem))
 
 #variável __name__ que indica a entrada do projeto
 #se o projeto veio pelo programa main então inicia o projeto
